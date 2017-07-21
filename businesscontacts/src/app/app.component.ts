76d8710b335e37afa9b7d23aac59c519d542b77d
mlwebdev <marvinllbiz@gmail.com>
2017-07-20 22:31:41 -0400

    Update AppComponent & AppModule
    Update AppComponent & AppModule

--- businesscontacts/src/app/app.component.ts
+++ businesscontacts/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  
+  constructor(private _firebaseService: FirebaseService) {
+  
+  }
+  ngOnInit() {
+    this._firebaseService.getBusinesses().subscribe(businesses => {
+      console.log(businesses);
+    });
+  }
 }
