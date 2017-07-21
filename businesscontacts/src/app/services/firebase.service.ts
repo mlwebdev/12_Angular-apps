69707f6b97cec955bc5d6fefccea298556e57888
mlwebdev <marvinllbiz@gmail.com>
2017-07-20 22:30:12 -0400

    Add Business.ts, Category.ts & Firebase Service
    Add Business.ts, Category.ts & Firebase Service

---
+++ businesscontacts/src/app/services/firebase.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import 'rxjs/add/operator/map';
+import { Business } from '../Business';
+import { Category } from '../Category';
+
+@Injectable()
+export class FirebaseService {
+  businesses: FirebaseListObservable<Business[]>;
+  categories: FirebaseListObservable<Category[]>;
+  
+  constructor(private _af: AngularFireDatabase){
+  }
+  
+  getBusinesses(){
+    this.businesses = this._af.list('/businesses') as
+    FirebaseListObservable<Business[]>;
+    return this.businesses;
+  }
+  
+  getCategories(){
+    this.categories = this._af.list('/categories') as
+      FirebaseListObservable<Category[]>;
+    return this.categories;
+  }
+}
